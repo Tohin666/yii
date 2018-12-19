0f853353ee9eae640652ccfd50309ab7ad99f60b
Tohin666 <tohin666@gmail.com>
2018-12-19 21:32:35 +0300

    start

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -12,12 +12,31 @@ class TaskController extends Controller
     {
 //        \Yii::$app->request->get('id');
 
-//        $model = new ContactForm();
-//        $model->name = 5;
-//
-//        var_dump($model->validate());
-//        var_dump($model->getErrors());
-//        exit;
+        $model = new ContactForm();
+
+//        // Каждый объект можно проинициализировать передав в конструктор массив с именами атрибутов и значениями.
+//        $model = new ContactForm([
+//            'name' => 'namedfadsdf',
+//            'email' => 'emailsdfasdf',
+//            'subject' => 'subgasdfasdf'
+//        ]);
+//        // тоже самое можно сделать после. Массив должен строго совпадать.
+//        $model->setAttributes([
+//            'name' => 'namedfadsdf',
+//            'email' => 'emailsdfasdf',
+//            'subject' => 'subgasdfasdf'
+//        ]);
+//        // тоже самое, только ищет и подставляем совпадения в указанном объекте. Массив может быть любым. Будут
+//        // загружены только те атрибуты, на которые назначена валидация.
+//        $model->load([
+//           'ContactForm' =>
+//               [
+//                   'name' => 'namedfadsdf',
+//                   'email' => 'emailsdfasdf',
+//                   'subject' => 'subgasdfasdf'
+//               ],
+//            'SomeOtherForm' => ['someData']
+//        ]);
 
         return $this->render('index', [
             'title' => 'Yii2',
